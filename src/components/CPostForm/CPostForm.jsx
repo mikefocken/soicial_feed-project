c73d66e454ad440981d08da34e1c51c23e245190
mike focken <mikefocken@gmail.com>
2023-12-12 15:37:01 -0600

    created handleSubmits

--- src/components/CPostForm/CPostForm.jsx
+++ src/components/CPostForm/CPostForm.jsx
@@ -1,12 +1,32 @@
-import './CPostForm.css'
-const PostList = ({}) => {
+import React, { useState } from "react";
+import "./CPostForm.css";
+
+const CPostForm = ({}) => {
+  const [name, setName] = useState("");
+  const [post, setPost] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData ={
+       name,
+       post
+    }
+    console.log(formData);
+  };
   return (
-    <div className="flex-item">
-      <h4>Name</h4>
-      <h4>Post</h4>
-      <button />
-    </div>
+    <form onSubmit={handleSubmit} className="flex-item">
+      <div>
+        <label>Name</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Post</label>
+        <input value={post} onChange={(e) => setPost(e.target.value)} />
+      </div>
+      <button type="submit"> Create</button>
+    </form>
   );
 };
 
-export default PostList;
+export default CPostForm;
