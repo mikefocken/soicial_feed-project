16046e22d04529447c1da62f4265fca8c6aa02af
mike focken <mikefocken@gmail.com>
2023-12-13 17:02:20 -0600

    working like and dislike buttons

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,38 @@
+import React, { useState } from "react";
+import './Post.css';
 
-const Post = ({postObj}) => {
+const Post = ({ postObj }) => {
+  const [isLike, setIsLike] = useState(false);
+  const [isDisLike, setIsDisLike] = useState(false);
 
-  console.log(postObj)
+  console.log(isLike);
+
+  const handleLikeClick = () => {
+    setIsLike(!isLike);
+  };
+
+  const handleDislikeClick = () => {
+    setIsDisLike(!isDisLike);
+  };
+
+  const btnLikeClass = isLike ? 'active-btn' : '';
+  const btnDisLikeClass = isDisLike ? 'active-btn' : '';
+
   return (
     <div>
-        <h4>{postObj.name}</h4>
+      <h4>{postObj.name}</h4>
       <div>
         <span>{postObj.postMessage}</span>
       </div>
+      <button className={btnLikeClass} onClick={handleLikeClick} type="button">
+        Like
+      </button>
+      <button className={btnDisLikeClass} onClick={handleDislikeClick} type="button">
+        Dislike
+      </button>
     </div>
   );
 };
 
 export default Post;
+
